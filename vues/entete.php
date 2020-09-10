6aa7fa49a7cde3dbcacb0786d4f01cdfb3e13d14
Marine Menoret <marine.menoret@gmail.com>
2020-09-10 14:32:45 -0400

    suppression du header dupliqué (bug)

--- vues/entete.php
+++ vues/entete.php
@@ -36,13 +36,4 @@
 </head>
 
 <body>
-	<header>
-		<h1>Un petit verre de vino ?</h1>
-		<nav>
-			<ul>
-				<li><a href="?requete=accueil">Mon cellier</a></li>
-				<li><a href="?requete=ajouterNouvelleBouteilleCellier">Ajouter une bouteille au cellier</a></li>
-			</ul>
-		</nav>
-	</header>
 	<main>
