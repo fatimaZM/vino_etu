5a3881ec1811989ce2e1ea108a5db93b2acd9229
Marine Menoret <marine.menoret@gmail.com>
2020-09-10 10:42:01 -0400

    modification de la fonction ajouterNouvelleBouteilleCellier() pour prendre en compte le cas d'une bouteille déjà existente

--- updateSAQ.php
+++ updateSAQ.php
@@ -1,25 +1,26 @@
 <!DOCTYPE HTML>
 <html>
-	
-	<head>
-		<meta charset="UTF-8" />	
-	</head>
-	<body>
-<?php
-	require("dataconf.php");
-	require("config.php");
-	$page = 1;
-	$nombreProduit = 24; //48 ou 96	
-	
-	$saq = new SAQ();
-	for($i=0; $i<1;$i++)	//permet d'importer séquentiellement plusieurs pages.
-	{
-		echo "<h2>page ". ($page+$i)."</h2>";
-		$nombre = $saq->getProduits($nombreProduit,$page+$i);
-		echo "importation : ". $nombre. "<br>";
-	
-	}
 
-?>
+<head>
+    <meta charset="UTF-8" />
+</head>
+
+<body>
+    <?php
+    require("dataconf.php");
+    require("config.php");
+    $page = 1;
+    $nombreProduit = 24; //48 ou 96	
+
+    $saq = new SAQ();
+    for ($i = 0; $i < 1; $i++)    //permet d'importer séquentiellement plusieurs pages.
+    {
+        echo "<h2>page " . ($page + $i) . "</h2>";
+        $nombre = $saq->getProduits($nombreProduit, $page + $i);
+        echo "importation : " . $nombre . "<br>";
+    }
+
+    ?>
 </body>
-</html>
+
+</html
