4f7d41a2d6c48a9ecdd1554a12f2a4cd2e34405b
Fatima Meftah <meftah.fatimazohra@gmail.com>
2020-09-17 22:45:26 -0400

    ajout logo, mon compte

--- vues/entete.php
+++ vues/entete.php
@@ -2,36 +2,51 @@
 <html lang="fr">
 
 <head>
-	<title>Un petit verre de vino</title>
+    <title>Un petit verre de vino</title>
 
-	<meta charset="utf-8">
-	<meta http-equiv="cache-control" content="no-cache">
-	<meta name="viewport" content="width=device-width, minimum-scale=0.5, initial-scale=1.0, user-scalable=yes">
+    <meta charset="utf-8">
+    <meta http-equiv="cache-control" content="no-cache">
+    <meta name="viewport" content="width=device-width, minimum-scale=0.5, initial-scale=1.0, user-scalable=yes">
 
-		<link rel="stylesheet" href="./css/normalize.css" type="text/css" media="screen">
-		<link rel="stylesheet" href="./css/base_h5bp.css" type="text/css" media="screen">
-		<link rel="stylesheet" href="./css/main.css" type="text/css" media="screen">
-		<base href="<?php echo BASEURL; ?>">
-		<!--<script src="./js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>-->
-	<!--	<script src="./js/plugins.js"></script> -->
-		<script src="./js/main.js"></script>
-	</head>
-	<body >
-		<header>
-			<h1>Un petit verre de vino ?</h1>
-			<nav>
-				<ul>
-					<li><a href="?requete=accueil">Mon cellier</a></li>
-					<li><a href="?requete=ajouterNouvelleBouteilleCellier">Ajouter une bouteille au cellier</a></li>
-				</ul>
-			</nav>
-		</header>
-		<main>
-			
-	<link rel="stylesheet" href="./css/normalize.css" type="text/css" media="screen">
-	<link rel="stylesheet" href="./css/base_h5bp.css" type="text/css" media="screen">
-	<link rel="stylesheet" href="./css/main.css" type="text/css" media="screen">
-	<base href="<?php echo BASEURL; ?>"> <!-- Specify a default URL and a default target for all links on a page -->
-	<script src="./js/main.js"></script>
+    <link rel="stylesheet" href="./css/normalize.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="./css/base_h5bp.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="./css/main.css" type="text/css" media="screen">
+    <base href="<?php echo BASEURL; ?>">
+    <script src="./js/main.js"></script>
+    <script src='https://kit.fontawesome.com/a076d05399.js'></script>
 </head>
 
+<body>
+    <header class="header">
+       
+         
+         <nav class="nav">
+         <label for="toggle">&#9776;</label>
+        <input type="checkbox" id="toggle"/>
+        <ul class="menu">
+            <li><a href="?requete=accueil">Mon cellier</a></li>
+            <li><a href="?requete=ajouterNouvelleBouteilleCellier">Ajouter une bouteille au cellier</a></li>
+            <li><a href="?requete=authentification">Se deconnecter</a></li>
+
+        </ul>
+        
+        
+    </nav>
+
+       <div class="compte-logo">
+          <a href="?requete=accueil" class="logo"><img src="./images/logo.png"></a>
+        <aside>
+            
+                <a href=""><i class='fas fa-user-alt'></i> Mon Compte</a>
+                <p>Nicolas Cage</p>
+                
+        </aside>
+      
+            </div>
+        
+    
+
+   
+</header>
+
+    <main>
