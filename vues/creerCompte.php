2b440d722431eb637f0ec4ed9c882631799fba1f
Fatima Meftah <meftah.fatimazohra@gmail.com>
2020-09-17 22:35:58 -0400

    vue creerCompte.php ajouté

---
+++ vues/creerCompte.php
@@ -0,0 +1,17 @@
+<form class="creer-compte">
+  <div class="container">
+    <h1>Mon compte</h1>
+    <p>Veuillez s'il vous plait remplir les champs suivants pour crééer un compte</p>
+    
+    <input type="text" name="prenom" placeholder="Entrer le prenom"  required>
+    <input type="text" name="nom" placeholder="Entrer le nom"  required>
+    <input type="text" name="identifiant" placeholder="Entrer l'identifiant"  required>
+    <input type="email" name="courriel" placeholder="Entrer le courriel"  required>
+    <input type="password" name="mdp" placeholder="Entrer le mot de passe"  required>
+
+    <div class="confirmer">
+      <button type="button" class="annuler">Annuler</button>
+      <button type="submit" class="confirmerCompte">Créer un compte</button>
+    </div>
+  </div>
+</form>
