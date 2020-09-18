04d977cb33a8a9c6652667bf74abc4de89c23d2a
Fatima Meftah <meftah.fatimazohra@gmail.com>
2020-09-17 21:18:44 -0400

    fichier authentification ajouté

---
+++ vues/authentification.php
@@ -0,0 +1,12 @@
+<div class="authentification">
+  <div class="form">
+  <p class="message">Nouvel utilisateur?<a href="?requete=ajouterNouvelUtilisateur">Créer un compte</a></p>
+    <form class="authentification-form" method="post">
+    <input type="email" id="courriel" name="courriel" placeholder="courriel" required/>
+      <input type="password" name ="mdp" placeholder="Mot de passe"  required/>
+
+      <button class="btnConnexion" type="submit">Se connecter</button>
+     
+    </form>
+  </div>
+</div>
